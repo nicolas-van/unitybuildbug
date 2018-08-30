c079d64f4be018cb75fe700bb224ad76bf3d028c
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2018-08-30 11:00:43 +0200

    Make install script handle errors

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,11 +1,7 @@
 #! /bin/sh
 
-# Example install script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
-
-# This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
-# installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
 echo 'Downloading'
-curl -o Unity.pkg https://netstorage.unity3d.com/unity/3071d1717b71/MacEditorInstaller/Unity-2018.2.5f1.pkg
+curl -o Unity.pkg https://netstorage.unity3d.com/unity/3071d1717b71/MacEditorInstaller/Unity-2018.2.5f1.pkg || exit 1
 
 echo 'Installing Unity.pkg'
-sudo installer -dumplog -package Unity.pkg -target /
+sudo installer -dumplog -package Unity.pkg -target / || exit 1
