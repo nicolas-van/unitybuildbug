a6310462ae4af1f2c026597815d7f6a765dbb8d0
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2018-08-30 11:50:29 +0200

    update build

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -5,14 +5,14 @@
 # Change this the name of your project. This will be the name of the final executables as well.
 project="ci-build"
 
-ls -al $(pwd)
+ls -al ./
 
-echo Attempting to build $project for OS X from $(pwd)
+echo Attempting to build $project for OS X
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
   -silent-crashes \
   -logFile /dev/stdout \
-  -projectPath "$(pwd)" \
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
+  -projectPath "./" \
+  -buildOSXUniversalPlayer "./Build/osx/$project.app" \
   -quit || exit 1
